95ce9800a59b3e87f8deadef9ab6b554818fd630
ridomin <ridomin@live.com>
2020-05-28 12:12:46 -0700

    more cli options

--- pnp-admin/cli/repo-admin.js
+++ pnp-admin/cli/repo-admin.js
@@ -1,5 +1,6 @@
 const inquirer = require('inquirer')
 const PluginManager = require('live-plugin-manager').PluginManager
+const repo = require('../app.modelRepo')
 
 const manager = new PluginManager({
   pluginsPath: 'dtdl_models',
@@ -7,23 +8,43 @@ const manager = new PluginManager({
 })
 
 inquirer.prompt([
-  { type: 'input', name: 'scope', question: 'scope', default: '@digital-twins' },
-  { type: 'input', name: 'pkgSearch', question: 'pkg to search', default: 'com-example-thermostat' }
-])
-  .then(answer => {
-    manager.queryPackage(answer.scope + '/' + answer.pkgSearch)
-      .then(pi => {
-        console.log('package found in registry %s %s', pi.name, pi.version)
-
-        inquirer.prompt([{ name: 'install', type: 'confirm' }])
-          .then(answer => {
-            if (answer.install) {
-              manager.install(pi.name, pi.version)
-                .then(ipi => {
-                  console.log(ipi.dependencies)
+  {
+    type: 'list',
+    name: 'operation',
+    choices: ['List Models', 'Add Models', 'Clean Models']
+  }
+]).then(async (answer) => {
+  switch (answer.operation) {
+    case 'List Models': {
+      const models = await repo.loadModelsFromFS()
+      models.forEach(m => console.log(m.id + ' -> ' + m.pkg.replace('./dtdl_models/', '')))
+      break
+    }
+    case 'Clean Models': {
+      console.log('not implemented')
+      break
+    }
+    case 'Add Models':
+      inquirer.prompt([
+        { type: 'input', name: 'scope', question: 'scope', default: '@digital-twins' },
+        { type: 'input', name: 'pkgSearch', question: 'pkg to search', default: 'com-example-thermostat' }
+      ])
+        .then(answer => {
+          manager.queryPackage(answer.scope + '/' + answer.pkgSearch)
+            .then(pi => {
+              console.log('package found in registry %s %s', pi.name, pi.version)
+              inquirer.prompt([{ name: 'install', type: 'confirm' }])
+                .then(answer => {
+                  if (answer.install) {
+                    manager.install(pi.name, pi.version)
+                      .then(ipi => {
+                        console.log(ipi.dependencies)
+                      })
+                  }
                 })
-            }
-          })
-      })
-      .catch(e => console.error(e))
-  })
+            })
+            .catch(e => console.error(e))
+        })
+      break
+  }
+})
