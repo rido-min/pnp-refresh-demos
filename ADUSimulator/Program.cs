ad8ebb9f97857542d5dfb6861a57dd89e0715389
ridomin <ridomin@live.com>
2020-07-16 16:52:50 -0700

    implement ADU simulator

---
+++ ADUSimulator/Program.cs
@@ -0,0 +1,14 @@
+﻿using ADUSimulator;
+using DeviceRunner;
+using System.Threading.Tasks;
+
+namespace ADUSimulator
+{
+  class Program
+  {
+    public static async Task Main(string[] args)
+    {
+      await ConsoleRunnerService<ADUSimulatorDevice>.RunDeviceAsync(args);
+    }
+  }
+}
